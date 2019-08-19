e144285f2752ffb75d7cab0aec516ca1d86ebfcf
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-19 16:03:46 -0500

    added test to check Dashboard for default state on first render

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,11 +1,19 @@
 // Test away
 import React from "react";
 import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
+
 import Dashboard from "./Dashboard";
 
 describe("<Dashboard />", () => {
   it("matches snapshot", () => {
     const tree = renderer.create(<Dashboard />);
     expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("defaults to open/unlocked on initial render", () => {
+    const { queryByText } = render(<Dashboard />);
+    expect(queryByText(/open/i)).toBeTruthy();
+    expect(queryByText(/unlocked/i)).toBeTruthy();
   });
 });
