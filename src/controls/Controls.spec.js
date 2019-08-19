d40de96a22590b6d78ee82ea4b9d0e3faa2d11e5
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-19 15:15:37 -0500

    added dependencies, created snapshot tests for each component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import renderer from "react-test-renderer";
+
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Controls />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import renderer from "react-test-renderer";
+
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Display />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
