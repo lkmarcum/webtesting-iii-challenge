2c8a309a0ffcd22e0837e4f5f0c26dfb1946b14c
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-19 15:27:54 -0500

    added test for function firing on button clicks in Controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -10,4 +10,22 @@ describe("<Controls />", () => {
     const tree = renderer.create(<Controls />);
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  it("fires a function when buttons are clicked", () => {
+    const mockFunc = jest.fn();
+    const locked = false;
+    const closed = false;
+    const { getByText } = render(
+      <Controls
+        locked={locked}
+        closed={closed}
+        toggleLocked={mockFunc}
+        toggleClosed={mockFunc}
+      />
+    );
+    fireEvent.click(getByText(/close gate/i));
+    expect(mockFunc).toHaveBeenCalled();
+    fireEvent.click(getByText(/lock gate/i));
+    expect(mockFunc).toHaveBeenCalled();
+  });
 });
