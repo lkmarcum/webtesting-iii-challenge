adb49465d257bb54f53fddddaced5e7a06144ed6
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-19 15:50:24 -0500

    added tests to check correct Display render based on props

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import renderer from "react-test-renderer";
 
 import Display from "./Display";
@@ -9,5 +9,17 @@ describe("<Display />", () => {
   it("matches snapshot", () => {
     const tree = renderer.create(<Display />);
     expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("correctly displays unlocked/open text based on props", () => {
+    const { queryByText } = render(<Display locked={false} closed={false} />);
+    expect(queryByText(/open/i)).toBeTruthy();
+    expect(queryByText(/unlocked/i)).toBeTruthy();
+  });
+
+  it("correctly displays locked/closed text based on props", () => {
+    const { queryByText } = render(<Display locked={true} closed={true} />);
+    expect(queryByText(/closed/i)).toBeTruthy();
+    expect(queryByText(/locked/i)).toBeTruthy();
   });
 });
